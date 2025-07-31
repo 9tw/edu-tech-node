6f6634539ae3e5c213119ba2b5b5d20b2740a8d2
Gerdo Wijaya <gerdo.tewel@gmail.com>
2025-07-31 17:04:05 +0900

    add crud to user

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { index } = require("../controllers/userController");
+const {
+  index,
+  create,
+  update,
+  destroy,
+} = require("../controllers/userController");
 
 router.get("/", index);
+router.post("/", create);
+router.put("/:id", update);
+router.delete("/:id", destroy);
 
 module.exports = router;
